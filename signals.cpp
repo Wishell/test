8e13b6251224b0234988b0078b752179fdc7b4d7
Wishell <wishell@mail.ru>
2018-02-16 08:42:13 -0500

    asda

--- signals.cpp
+++ signals.cpp
@@ -8,19 +8,17 @@
 int main (int argc, char** argv){
 
 std::ofstream fpar;
-fpar.open(" /home/osboxes/sig/pid_parent.txt");
 std::ofstream fchld;
-fchld.open(" /home/osboxes/sig/pid_child.txt");
 
 pid_t pid_par;
 pid_t pid_chld;
 
-pid_par = getpid();
-fpar << pid_par;
-fpar.close();
-
 if (fork()){
 //parent
+pid_par = getpid();
+fpar.open("/home/osboxes/sig/pid_parent.txt");
+fpar << pid_par;
+fpar.close();
 printf("parent proc pid=%d \n",pid_par);
 signal(SIGTERM,SIG_IGN);
 signal(SIGABRT,SIG_IGN);
@@ -32,6 +30,7 @@ sleep(1);
 } else {
 //chld
 pid_chld = getpid();
+fchld.open("/home/osboxes/sig/pid_child.txt");
 fchld << pid_chld;
 fchld.close();
 printf("child proc pid=%d \n",pid_chld);
